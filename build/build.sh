d4208e25da691a926fb74c3fa2a404a7303267ba
xuzhuoxi <xuzhuoxi@gmail.com>
2019-04-30 02:18:34 +0800

    1.构造脚本 2.Demo更新

---
+++ build/build.sh
@@ -0,0 +1,1 @@
+goxc -os="linux darwin windows freebsd openbsd" -arch="386 amd64 arm" -n=IconGen -pv=1.0 -wd=../src -d=./release -include=*.go
--- demo/win/cmd.bat
+++ demo/win/cmd.bat
@@ -1,2 +1,3 @@
 IconGen.exe -size=32,64,128,256,512 -in=./icon/source -out=./icon/target -format=png -ratio=75
+
 pause
