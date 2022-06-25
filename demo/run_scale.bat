e962a36de76466aa8d1226a7f1bf8874c9e7f9fd
xuzhuoxi <xuzhuoxi@gmail.com>
2022-06-25 22:30:14 +0800

    1.增加demo资源

---
+++ demo/run_scale.bat
@@ -0,0 +1,6 @@
+:: 单个产出
+ImageResizer_win_amd64.exe -mode=scale -src=src/SrcIcon.png -tar_file=tar/scale/TarIcon.png -scale=0.8 -format=jpg -ratio=85
+:: 批量产出
+ImageResizer_win_amd64.exe -mode=scale -include=png,jpg -src=src/dir,src/SrcIcon.png -tar_dir=tar/scale -scale=0.6,1,1.2 -ratio=85
+
+pause
--- demo/win/cmd.bat
+++
@@ -1,9 +0,0 @@
-:: IconGen.exe -size=32 -in=./icon/source/icon.png -inFolder=false -out=./icon/target/icon32x32.png -outFolder=false -format=png -ratio=75
-
-:: IconGen.exe -size=32,64,128,256x256,512 -in=./icon/source/icon.png -inFolder=false -out=./icon/target/ -outFolder=true -format=png -ratio=75
-
-:: IconGen.exe -size=32,64,128,256x256,512 -in=./icon/source/ -inFolder=true -out=./icon/target/ -outFolder=true -format=png -ratio=75
-
-IconGen.exe -size=32,64,128,256x256,512 -in=./icon/source -inFolder=true -out=./icon/target/ -outFolder=true -ratio=100
-
-pause
